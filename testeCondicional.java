1308efc358be1c453d291596a8a1fc7279e46680
Talilo Tarlison <101112484+talilotarlison@users.noreply.github.com>
2023-06-01 15:22:12 -0300

    Create testeCondicional.java

---
+++ testeCondicional.java
@@ -0,0 +1,16 @@
+//Teste de ondicional
+public class MyClass {
+    public static void main(String args[]) {
+       int baterias = 3;
+       int distanciaPercorrida = 267;
+       float percurso = ( 100 * baterias);
+        
+        if (distanciaPercorrida <= percurso) {
+            System.out.println("você tem bateria suficiente para percorer");
+        }else{ 
+             System.out.println("você nao tem autonimia para o percurso");
+            
+        }
+    }
+}
+ 
