119ed029222b8acd85a2aabc87599427482b9447
Talilo Tarlison <101112484+talilotarlison@users.noreply.github.com>
2023-06-01 16:47:55 -0300

    Update heranca_poo.js

--- heranca_poo.js
+++ heranca_poo.js
@@ -24,3 +24,32 @@ var muse = new Formato(155, "Panic Station", "mp3","4min");
 console.log(muse);
 
 //https://www.w3schools.com/jsref/jsref_class_extends.asp
+
+//teste 
+
+class Musica {
+  nome;
+  id;
+  constructor(id,name){
+    this.name = name;
+    this.id = id;
+  }
+}
+var  Adele = new Musica(155, "Set fire");
+console.log(Adele);
+
+class Formato extends Musica{
+  tipo;
+  time;
+   constructor(id, nome ,tipo,time){
+     super(id,nome);
+    this.tipo = tipo;
+     this.time = time;
+   }
+}
+
+var muse = new Formato(155, "Panic Station", "mp3","4min");
+
+console.log(muse);
+
+//https://www.w3schools.com/jsref/jsref_class_extends.asp
