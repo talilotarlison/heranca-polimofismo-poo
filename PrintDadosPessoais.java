60989d15a2902f9c3a259217f728c44bb7235c79
Talilo Tarlison <101112484+talilotarlison@users.noreply.github.com>
2023-10-21 07:22:22 -0300

    Create PrintDadosPessoais.java

---
+++ PrintDadosPessoais.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      System.out.println("Carregando dados...");
+     dadosPessoais();
+  }
+
+
+static void dadosPessoais() {
+        System.out.println("Nome: Nelson Silva");
+        System.out.println("Idade: 25");
+        System.out.println("Nacionalidade: Portuguesa");
+      }
+    
+}
